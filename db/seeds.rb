71ff5db2fb2d6dcf6f6d7d65d5611050373b508f
Rahul Sharma <rshar24@gmail.com>
2019-05-22 14:25:50 +0800

    Basic functionality - add & view restaurants & reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Destroying all restaurants"
+Restaurant.destroy_all
+
+puts "Creating new restaurants"
+10.times do
+  restaurant = Restaurant.create(
+    name: Faker::Superhero.name,
+    address: Faker::Address.city,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+end
+puts "Finished!"
